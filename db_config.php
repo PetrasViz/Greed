e9926d7ddf79eaa8d50201dfce5e4b53435edc86
Petras Vizgintas <38919361+PetrasViz@users.noreply.github.com>
2024-09-11 10:45:15 +0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -5,7 +5,7 @@ $username = "u252309147_greedyadmin";
 $password = "?j8M>M=2Zgi";
 $dbname = "u252309147_greedloot";
 
-// Create connection
+// Create connection TEST
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
